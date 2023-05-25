496333de20bf1206cde5165bab041d173ed86b42
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2023-05-25 23:59:19 +0530

    inital commit

---
+++ Project 2/script.js
@@ -0,0 +1,37 @@
+const boxes = document.querySelectorAll(".box");
+
+//Function version
+/*
+window.addEventListener("scroll", checkBoxes);
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
+
+*/
+
+//Arrow function
+window.addEventListener("scroll", () => {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+});
