eda799e56c106daac30d13e922bdf74d0589159e
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2023-06-24 10:51:32 +0530

    inital commit

---
+++ Project 22/script.js
@@ -0,0 +1,40 @@
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+console.log(slidesLength);
+
+let activeslideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+
+  if (direction === "up") {
+    activeslideIndex++;
+    if (activeslideIndex > slidesLength - 1) {
+      activeslideIndex = 0;
+    }
+  }
+
+  if (direction === "down") {
+    activeslideIndex--;
+    if (activeslideIndex < 0) {
+      activeslideIndex = slidesLength - 1;
+    }
+  }
+  slideRight.style.transform = `translateY(-${
+    activeslideIndex * sliderHeight
+  }px)`;
+
+  slideLeft.style.transform = `translateY(${
+    activeslideIndex * sliderHeight
+  }px)`;
+};
