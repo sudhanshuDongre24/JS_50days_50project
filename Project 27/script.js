aea4c2086da5eeda9606d86766f64da770e2f4ff
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2023-07-01 18:47:28 +0530

    poster bug updated

--- Project 27/script.js
+++ Project 27/script.js
@@ -17,20 +17,30 @@ async function getMovies(url) {
   showMovies(data.results);
 }
 
+function imgLoader(imglink) {
+  const img =
+    imglink == null
+      ? "https://www.shutterstock.com/image-vector/movie-night-concept-creative-template-600w-1055348723.jpg"
+      : imgPath + imglink;
+
+  return img;
+}
+
 function showMovies(movies) {
   main.innerHTML = "";
 
   movies.forEach((movie) => {
     const { title, poster_path, vote_average, overview } = movie;
+    console.log(imgPath + poster_path);
 
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
 
     movieEl.innerHTML = `
-        <img src="${imgPath + poster_path}"/ alt="${title}" >
+        <img src="${imgLoader(poster_path)}" alt="${title}" >
         <div class="movie-info">
             <h3>${title}</h3>
-            <span class="#{getClassByRate(vote_average)}">${vote_average}</span>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
         <div>
 
         <div class="overview">
