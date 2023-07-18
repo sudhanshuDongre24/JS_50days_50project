7b137cc5f5288faec8282a53f150cc0f013ad143
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2023-07-18 19:57:25 +0530

    inital commit

---
+++ Project 39/script.js
@@ -0,0 +1,41 @@
+const result = document.getElementById("result");
+const filter = document.getElementById("filter");
+const listItem = [];
+
+getData();
+
+filter.addEventListener("input", (e) => filterData(e.target.value));
+
+async function getData() {
+  const res = await fetch("https://randomuser.me/api/?results=500&nat=IN");
+  const { results } = await res.json();
+
+  result.innerHTML = "";
+  //   console.log(results);
+
+  results.forEach((user) => {
+    const li = document.createElement("li");
+
+    listItem.push(li);
+
+    li.innerHTML = `
+    <img src = "${user.picture.large}" alt="${user.name.first}">
+    <div class="user-info">
+        <h4>${user.name.first} ${user.name.last}</h4>
+        <p>${user.location.city}, ${user.location.country}</p>
+    </div>
+    `;
+
+    result.appendChild(li);
+  });
+}
+
+function filterData(searchTerm) {
+  listItem.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove("hide");
+    } else {
+      item.classList.add("hide");
+    }
+  });
+}
