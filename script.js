2df4b8c4719f44d9b1d5191d2fd30bd6300de4e0
Sudhanshu <133798904+sudhanshuDongre24@users.noreply.github.com>
2024-03-02 14:13:31 +0530

    Add files via upload
    update files

---
+++ script.js
@@ -0,0 +1,64 @@
+const container = document.querySelector(".projectSection");
+
+const arr = [
+  `Project 1 - Progrss Steps`,
+  "Project 2 - Scroll Animation",
+  "Project 3 - Form Input Wave",
+  "Project 4 - Random Choice Picker",
+  "Project 5 - Drink Water",
+  "Project 6 - Theme Clock",
+  "Project 7 - Auto Text Effect",
+  "Project 8 - Increment Counter",
+  "Project 9 - Good, Cheap, Fast",
+  "Project 10 - Button Ripple Effect",
+  "Project 11 - Animated Countdown",
+  "Project 12 - Kinetic Loader",
+  "Project 13 - Animated Navigation",
+  "Project 14 - 3D Boxes Background",
+  "Project 15 - Background Slider",
+  "Project 16 - Custom Range Sldier",
+  "Project 17 - Drag N Drop",
+  "Project 18 - Expanding Cards",
+  "Project 19 - HoverBoard",
+  "Project 20 - Blurry Loading",
+  "Project 21 - Content PlaceHolder",
+  "Project 22 - Double Vertical Slider",
+  "Project 23 - Double Click Heart",
+  "Project 24 - Dad Jokes",
+  "Project 25 - Let us Know Your Feedback",
+  "Project 26 - Github Profile",
+  "Project 27 - Movie App",
+  "Project 28 - ToDo list",
+  "Project 29 - Image Carousel",
+  "Project 30 - Mobile Tab Navigato",
+  "Project 31 -Netlfix Mobile Navigator",
+  "Project 32 - Notes App",
+  "Project 33 - Password Generator",
+  "Project 34 - Drawing App",
+  "Project 35 - Event keyCodes",
+  "Project 36 - FAQ Collapse",
+  "Project 37 - Hiddden Search",
+  "Project 38 - Insect Catch Game",
+  "Project 39 - Live user filter",
+  "Project 40 - Pssword Strength Background",
+  "Project 41 - PokeDex",
+  "Project 42 - Quiz App",
+  "Project 43 - Random Image Generator",
+  "Project 44 - Rotating Nav Animation",
+  "Project 45 - Sound Board",
+  "Project 46 - Split Landing Page",
+  "Project 47 - Sticky Navigation",
+  "Project 48 - Testimonial Box Switcher",
+  "Project 49 - Toast Notification",
+  "Project 50 - Verify account UI",
+];
+
+arr.forEach((project, idx) => {
+  const h2 = document.createElement("h2");
+  const a = document.createElement("a");
+  a.setAttribute("href", `/Project ${idx + 1}/index.html`);
+  a.textContent = project;
+
+  h2.appendChild(a);
+  container.appendChild(h2);
+});
